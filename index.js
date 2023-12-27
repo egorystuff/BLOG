10b9cab5bc770273dc49b6dbeea33261412e1881
Yahor Yankavets <mr.yankovec@gmail.com>
2023-12-27 16:43:20 +0300

    new update

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+mongoose
+  .connect("mongodb+srv://admin:lantirn1994@cluster0.mt5aysl.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hi");
-});
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty) {
+    return res.status(400).json(errors.array());
+  }
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fillName: "Василий лох",
-    },
-    "secret123",
-  );
-
-  res.json({
-    success: true,
-    token,
-  });
+  res.json({ success: true });
 });
 
 app.listen(4444, (err) => {
   if (err) return console.log(err);
-  return console.log(`Server started on port ${4444}`);
+  return console.log(`Server OK`);
 });
